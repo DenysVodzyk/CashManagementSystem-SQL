2d1370127f0711bc13421110ca6be9475eecd419
DenysVodzyk <vodzyk@gmail.com>
2021-02-07 13:42:12 -0500

    Simplified class based on new PaymentService class functionality

--- src/ReportService/MerchantReportService.java
+++ src/ReportService/MerchantReportService.java
@@ -1,47 +1,41 @@
 package ReportService;
 
-import Entity.Merchant;
 import Entity.Payment;
-import Service.MerchantService;
 import Service.PaymentService;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class MerchantReportService {
-    MerchantService merchantService = new MerchantService();
     PaymentService paymentService = new PaymentService();
 
     public MerchantReportService() throws IOException, SQLException {
-        merchantService.readFromDB();
         paymentService.readFromDB();
     }
 
     public String totalSumPaid(int id) {
-        Merchant merchant = merchantService.getById(id);
-        int merchant_id = merchant.getId();
-        String title = merchant.getName();
-        LocalDate lastSent = merchant.getLastSent();
+        String title = "";
+        LocalDate lastSent = null;
         double totalSum = 0;
 
         for (Payment payment : paymentService.getPayments()) {
-            if (payment.getMerchantId() == id)
+            if (payment.getMerchantId() == id) {
+                title = payment.getMerchant().getName();
+                lastSent = payment.getMerchant().getLastSent();
                 totalSum += payment.getSumPaid();
+            }
         }
-        return "Total sum paid: " + totalSum + ". Merchant Id: " + merchant_id + ", title: " + title
+        return "Total sum paid: " + totalSum + ". Merchant Id: " + id + ", title: " + title
                 + ", last sent: " + lastSent;
     }
 
-    public ArrayList<String> sortedDescendingOrder() {
-        ArrayList<String> merchantsByName = new ArrayList<>();
-        for (Merchant merchant : merchantService.getMerchants()) {
-            merchantsByName.add(merchant.getName());
-        }
-        Collections.sort(merchantsByName, Collections.reverseOrder());
-        return merchantsByName;
+    public TreeSet<String> sortedDescendingOrder() {
+        TreeSet<String> merchantsByName = new TreeSet<>();
+        for (Payment payment : paymentService.getPayments())
+            merchantsByName.add(payment.getMerchant().getName());
+        TreeSet<String> merchantsByNameDescend = (TreeSet<String>) merchantsByName.descendingSet();
+        return merchantsByNameDescend;
     }
-
 }
