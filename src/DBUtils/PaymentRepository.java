5a8e2c64801da56928b46ac6aee9f059a9b96d81
DenysVodzyk <vodzyk@gmail.com>
2021-02-08 19:31:35 -0500

    Rename and addPayment method refactor so it can accept Payment as param

--- src/DBUtils/PaymentRepository.java
+++ src/DBUtils/PaymentRepository.java
@@ -1,23 +1,27 @@
 package DBUtils;
 
+import Entity.Payment;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class PaymentTableService {
+public class PaymentRepository {
 
     //Lesson 7, clause 4
-    public void addPayment(java.sql.Timestamp dt, int merchantId, int customerId, String goods, double sumPaid, double chargePaid) throws IOException, SQLException {
+
+    public void addPayment(Payment payment) throws IOException, SQLException {
         Connection con = DBConnection.getConnection();
         String sql = "INSERT INTO payment(dt, merchantId, customerId, goods, sumPaid, chargePaid) VALUES (?,?,?,?,?,?)";
         PreparedStatement stm = con.prepareStatement(sql);
+        java.sql.Timestamp dt = java.sql.Timestamp.valueOf(payment.getDt());
         stm.setTimestamp(1, dt);
-        stm.setInt(2, merchantId);
-        stm.setInt(3, customerId);
-        stm.setString(4, goods);
-        stm.setDouble(5, sumPaid);
-        stm.setDouble(6, chargePaid);
+        stm.setInt(2, payment.getMerchantId());
+        stm.setInt(3, payment.getCustomerId());
+        stm.setString(4, payment.getGoods());
+        stm.setDouble(5, payment.getSumPaid());
+        stm.setDouble(6, payment.getChargePaid());
         stm.executeUpdate();
         stm.close();
     }
