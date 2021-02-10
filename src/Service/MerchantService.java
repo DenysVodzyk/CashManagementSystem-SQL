3833b5938ff5176b0a287b447d821864b270aa0b
DenysVodzyk <vodzyk@gmail.com>
2021-02-09 21:18:17 -0500

    Removed functionality related to DB read

--- src/Service/MerchantService.java
+++ src/Service/MerchantService.java
@@ -1,54 +1,25 @@
 package Service;
 
-import DBUtils.DBConnection;
+import DBUtils.MerchantRepository;
 import Entity.Merchant;
 
-import java.io.IOException;
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MerchantService {
-    private List<Merchant> merchants = new ArrayList<>();
+    private MerchantRepository merchantRepository = new MerchantRepository();
 
-    public boolean readFromDB() throws IOException, SQLException {
-        Connection con = DBConnection.getConnection();
-        String sql = "SELECT * FROM merchant";
-        PreparedStatement stm = con.prepareStatement(sql);
-        ResultSet rs = stm.executeQuery();
-
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String bankName = rs.getString("bankName");
-            String swift = rs.getString("swift");
-            String account = rs.getString("account");
-            double charge = rs.getDouble("charge");
-            int period = rs.getInt("period");
-            double minSum = rs.getDouble("minSum");
-            double needToSend = rs.getDouble("needToSend");
-            double sent = rs.getDouble("sent");
-            Date lastSentDate = rs.getDate("lastSent");
-            LocalDate lastSent = lastSentDate == null ? null : lastSentDate.toLocalDate();
-
-            merchants.add(new Merchant(id, name, bankName, swift, account, charge, period, minSum, needToSend, sent, lastSent));
-        }
-        return !merchants.isEmpty();
-    }
-
-    public List<Merchant> getMerchants() {
-        return merchants;
+    public List<Merchant> getAll() {
+        return merchantRepository.getAll();
     }
 
     public Merchant getById(int id) {
-        Merchant merchantToReturn = null;
-        for (Merchant merchant : merchants) {
+        Merchant merchantById = null;
+        for (Merchant merchant : getAll()) {
             if (merchant.getId() == id) {
-                merchantToReturn = merchant;
+                merchantById = merchant;
             }
         }
-        return merchantToReturn;
+        return merchantById;
     }
 
 }
