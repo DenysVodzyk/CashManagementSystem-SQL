65652dd835e127d2ecae2346b52bab696dd7d51e
DenysVodzyk <vodzyk@gmail.com>
2021-02-09 21:18:02 -0500

    Removed functionality related to DB read

--- src/Service/CustomerService.java
+++ src/Service/CustomerService.java
@@ -1,52 +1,26 @@
 package Service;
 
-import DBUtils.DBConnection;
+import DBUtils.CustomerRepository;
 import Entity.Customer;
 
-import java.io.IOException;
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class CustomerService {
-    private List<Customer> customers = new ArrayList<>();
+    private CustomerRepository customerRepository = new CustomerRepository();
 
-    public boolean readFromDB() throws IOException, SQLException {
-        Connection con = DBConnection.getConnection();
-        String sql = "SELECT * FROM customer";
-        PreparedStatement stmt = con.prepareStatement(sql);
-        ResultSet rs = stmt.executeQuery();
-
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String address = rs.getString("address");
-            String email = rs.getString("email");
-            String ccNo = rs.getString("ccNo");
-            String ccType = rs.getString("ccType");
-            Date maturityDate = rs.getDate("maturity");
-            LocalDate maturity = maturityDate == null ? null : maturityDate.toLocalDate();
-
-            customers.add(new Customer(id, name, address, email, ccNo, ccType, maturity));
-        }
-        con.close();
-        return !customers.isEmpty();
-    }
-
-    public List<Customer> getCustomers() {
-        return customers;
+    public List<Customer> getAll() {
+        return customerRepository.getAll();
     }
 
     public Customer getById(int id) {
-        Customer customerToReturn = null;
-        for (Customer customer : customers) {
+        Customer customerById = null;
+        for (Customer customer : getAll()) {
             if (customer.getId() == id) {
-                customerToReturn = customer;
+                customerById = customer;
             }
         }
-        return customerToReturn;
+        return customerById;
     }
 
 }
