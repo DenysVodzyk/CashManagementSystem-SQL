85e93242f384d36925db558dcb9ea7fead0824ce
DenysVodzyk <vodzyk@gmail.com>
2021-02-11 11:38:51 -0500

    Modified totalSumPaid to accept Merchant and changed sort method to make it more versatile

--- src/ReportService/MerchantReportService.java
+++ src/ReportService/MerchantReportService.java
@@ -1,5 +1,6 @@
 package ReportService;
 
+import Entity.Merchant;
 import Entity.Payment;
 import Service.PaymentService;
 
@@ -10,28 +11,32 @@ public class MerchantReportService {
     PaymentService paymentService = new PaymentService();
 
     // Lesson 7 - clause 2
-    public String totalSumPaid(int id) {
+    public String totalSumPaid(Merchant merchant) {
         String title = "";
         LocalDate lastSent = null;
         double totalSum = 0;
 
         for (Payment payment : paymentService.getAll()) {
-            if (payment.getMerchantId() == id) {
+            if (payment.getMerchantId() == merchant.getId()) {
                 title = payment.getMerchant().getName();
                 lastSent = payment.getMerchant().getLastSent();
                 totalSum += payment.getSumPaid();
             }
         }
-        return "Total sum paid: " + totalSum + ". Merchant Id: " + id + ", title: " + title
+        return "Total sum paid: " + totalSum + ". Merchant Id: " + merchant.getId() + ", title: " + title
                 + ", last sent: " + lastSent;
     }
 
     //Lesson 7 - Clause 3
-    public Set<String> sortedDescendingOrder() {
-        Set<String> merchantsByName = new TreeSet<>();
+    public Set<String> sortBasedOnOrder(String sortingOrder) {
+        Set<String> merchants = new TreeSet<String>();
+
         for (Payment payment : paymentService.getAll())
-            merchantsByName.add(payment.getMerchant().getName());
-        Set<String> merchantsByNameDescend = ((TreeSet<String>) merchantsByName).descendingSet();
-        return merchantsByNameDescend;
+            merchants.add(payment.getMerchant().getName());
+
+        if (sortingOrder.toLowerCase().contains("desc"))
+            return ((TreeSet<String>) merchants).descendingSet();
+
+        return merchants;
     }
 }
