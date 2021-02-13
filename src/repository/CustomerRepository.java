f3ff4cd53698ca2c20771099c659067d35bc2047
DenysVodzyk <vodzyk@gmail.com>
2021-02-12 20:45:48 -0500

    Methods simplification

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -1,7 +1,7 @@
-package Repository;
+package repository;
 
-import DBUtils.DBConnection;
-import Entity.Customer;
+import dBUtils.DBConnection;
+import entity.Customer;
 
 import java.io.IOException;
 import java.sql.*;
@@ -10,16 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerRepository {
-    List<Customer> customers = new ArrayList<>();
-    String sql = "SELECT * FROM customer";
 
-    public List<Customer> getAll() {
-
+    public Customer getById(int id) {
+        Customer customer = null;
+        String sql = "SELECT * FROM customer WHERE id=" + id;
         try (Connection con = DBConnection.getConnection();
              PreparedStatement stm = con.prepareStatement(sql)) {
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String address = rs.getString("address");
                 String email = rs.getString("email");
@@ -28,7 +26,25 @@ public class CustomerRepository {
                 Date maturityDate = rs.getDate("maturity");
                 LocalDate maturity = maturityDate == null ? null : maturityDate.toLocalDate();
 
-                customers.add(new Customer(id, name, address, email, ccNo, ccType, maturity));
+                customer = new Customer(id, name, address, email, ccNo, ccType, maturity);
+            }
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+        return customer;
+    }
+
+
+    public List<Customer> getAll() {
+        List<Customer> customers = new ArrayList<>();
+        String sql = "SELECT * FROM customer";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement stm = con.prepareStatement(sql)) {
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                Customer customer = getById(id);
+                customers.add(customer);
             }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
