bd8a10d1c8331cc516dec6b52304995d5bd5c80f
DenysVodzyk <vodzyk@gmail.com>
2021-02-13 23:20:32 -0500

    Methods refactor to reduce code redundancy

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -11,6 +11,19 @@ import java.util.List;
 
 public class CustomerRepository {
 
+    public Customer getCustomer(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String address = rs.getString("address");
+        String email = rs.getString("email");
+        String ccNo = rs.getString("ccNo");
+        String ccType = rs.getString("ccType");
+        Date maturityDate = rs.getDate("maturity");
+        LocalDate maturity = maturityDate == null ? null : maturityDate.toLocalDate();
+
+        return new Customer(id, name, address, email, ccNo, ccType, maturity);
+    }
+
     public Customer getById(int id) {
         Customer customer = null;
         String sql = "SELECT * FROM customer WHERE id=" + id;
@@ -18,22 +31,13 @@ public class CustomerRepository {
              PreparedStatement stm = con.prepareStatement(sql)) {
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
-                String name = rs.getString("name");
-                String address = rs.getString("address");
-                String email = rs.getString("email");
-                String ccNo = rs.getString("ccNo");
-                String ccType = rs.getString("ccType");
-                Date maturityDate = rs.getDate("maturity");
-                LocalDate maturity = maturityDate == null ? null : maturityDate.toLocalDate();
-
-                customer = new Customer(id, name, address, email, ccNo, ccType, maturity);
+                customer = getCustomer(rs);
             }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
         return customer;
     }
-
 
     public List<Customer> getAll() {
         List<Customer> customers = new ArrayList<>();
@@ -42,9 +46,7 @@ public class CustomerRepository {
              PreparedStatement stm = con.prepareStatement(sql)) {
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt("id");
-                Customer customer = getById(id);
-                customers.add(customer);
+                customers.add(getCustomer(rs));
             }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
