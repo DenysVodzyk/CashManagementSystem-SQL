9b10372b8b85b3f97d0dac2f3ff80fd357190fe5
DenysVodzyk <vodzyk@gmail.com>
2021-02-15 17:15:43 -0500

    Refactor based on new CustomerRepository functionality

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,26 +1,37 @@
-package Service;
+package service;
 
-import Repository.CustomerRepository;
-import Entity.Customer;
+import repository.CustomerRepository;
+import entity.Customer;
+import repository.MerchantRepository;
+import repository.PaymentRepository;
 
 import java.util.List;
 
-
 public class CustomerService {
-    private CustomerRepository customerRepository = new CustomerRepository();
+    private CustomerRepository customerRepository;
+    private MerchantRepository merchantRepository;
+    private PaymentRepository paymentRepository;
+
+    public CustomerService() {
+        this.customerRepository = new CustomerRepository();
+        this.merchantRepository = new MerchantRepository();
+        this.paymentRepository = new PaymentRepository();
+
+        this.customerRepository.setPaymentRepository(paymentRepository);
+        this.paymentRepository.setCustomerRepository(customerRepository);
+        this.paymentRepository.setMerchantRepository(merchantRepository);
+    }
 
     public List<Customer> getAll() {
         return customerRepository.getAll();
     }
 
     public Customer getById(int id) {
-        Customer customerById = null;
-        for (Customer customer : getAll()) {
-            if (customer.getId() == id) {
-                customerById = customer;
-            }
-        }
-        return customerById;
+        return customerRepository.getById(id, false);
     }
 
 }
+
+//
+//    private PaymentRepository paymentRepository = new PaymentRepository(cR, mR);
+//    private CustomerRepository customerRepository = new CustomerRepository(paymentRepository);
