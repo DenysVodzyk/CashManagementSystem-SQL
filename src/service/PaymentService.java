57204376661c87037287eb47dd91909abff3528c
DenysVodzyk <vodzyk@gmail.com>
2021-02-15 17:16:45 -0500

    Refactor based on new PaymentRepository functionality

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -1,15 +1,30 @@
-package Service;
+package service;
 
-import Repository.PaymentRepository;
-import Entity.Payment;
+import repository.CustomerRepository;
+import repository.MerchantRepository;
+import repository.PaymentRepository;
+import entity.Payment;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentService {
+    private CustomerRepository customerRepository;
+    private MerchantRepository merchantRepository;
+    private PaymentRepository paymentRepository;
+    private MerchantService merchantService;
 
-    private PaymentRepository paymentRepository = new PaymentRepository();
+    public PaymentService() {
+        this.customerRepository = new CustomerRepository();
+        this.merchantRepository = new MerchantRepository();
+        this.paymentRepository = new PaymentRepository();
+
+        this.customerRepository.setPaymentRepository(paymentRepository);
+        this.paymentRepository.setCustomerRepository(customerRepository);
+        this.paymentRepository.setMerchantRepository(merchantRepository);
+        this.merchantService = new MerchantService();
+    }
 
     public List<Payment> getAll() {
         return paymentRepository.getAll();
@@ -24,4 +39,17 @@ public class PaymentService {
         }
         return paymentsFromTimeInterval;
     }
+
+    public boolean addPayment(Payment payment) {
+        payment.getMerchant().setNeedToSend(merchantService.calculateNeedToSend(payment));
+        payment.setChargePaid(payment.getSumPaid() * payment.getMerchant().getCharge() / 100);
+        paymentRepository.addPayment(payment);
+        return true;
+    }
 }
+
+
+//    private CustomerRepository cR = new CustomerRepository();
+//    private MerchantRepository mR = new MerchantRepository();
+//
+//    private PaymentRepository paymentRepository = new PaymentRepository(cR, mR);
