547c21d659a7d81567dda82b4aa2b185f71ad8e0
DenysVodzyk <vodzyk@gmail.com>
2021-02-15 17:09:46 -0500

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        PrintReport printReport = new PrintReport();
+        Execute printReport = new Execute();
         printReport.execute();
 
     }
