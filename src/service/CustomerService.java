879a8d7631132d966091a21da0464b6ca48ae0a1
DenysVodzyk <vodzyk@gmail.com>
2021-02-21 21:30:54 -0500

    addCustomer method refactor so it account for duplicate entry

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -7,22 +7,18 @@ import repository.PaymentRepository;
 import utils.FileReader;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class CustomerService {
     private CustomerRepository customerRepository;
     private MerchantRepository merchantRepository;
     private PaymentRepository paymentRepository;
-    private FileReader fileReader;
 
     public CustomerService() {
         this.customerRepository = new CustomerRepository();
         this.merchantRepository = new MerchantRepository();
         this.paymentRepository = new PaymentRepository();
-        this.fileReader = new FileReader();
 
         this.customerRepository.setPaymentRepository(paymentRepository);
         this.paymentRepository.setCustomerRepository(customerRepository);
@@ -38,7 +34,7 @@ public class CustomerService {
     }
 
     public List<Customer> getCustomersFromFile(String filePath) {
-        List<String> data = fileReader.readFromFile(filePath);
+        List<String> data = FileReader.readFromFile(filePath);
         List<Customer> customers = new ArrayList<>();
 
         for (String customer : data) {
@@ -48,19 +44,22 @@ public class CustomerService {
             String email = customerData[2];
             String ccNo = customerData[3];
             String ccType = customerData[4];
-            LocalDate maturity = setMaturityDate(customerData[5]);
+            LocalDate maturity = FileReader.setLocalDate(customerData[5], "yyyy/MM/dd");
 
             customers.add(new Customer(name, address, email, ccNo, ccType, maturity));
         }
         return customers;
     }
 
-    private LocalDate setMaturityDate(String maturityDate) {
-        if (maturityDate != null) {
-            return null;
+    public void addCustomer(String filePath) {
+        List<Customer> customersFromFile = getCustomersFromFile(filePath);
+
+        for (Customer customer : customersFromFile) {
+            if (!getAll().contains(customer)) {
+                customerRepository.addCustomer(customer);
+            }
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate date = LocalDate.parse(maturityDate, formatter);
-        return date;
     }
+
+
 }
