b99ab48b5dd1db04a5a3a240d98d89d80aa9a5cd
DenysVodzyk <vodzyk@gmail.com>
2021-02-21 21:32:55 -0500

    added setLocalDate method to set a date from string based on the formatter

--- src/utils/FileReader.java
+++ src/utils/FileReader.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-    public List<String> readFromFile(String path) {
+    public static List<String> readFromFile(String path) {
         List<String> fileContent = new ArrayList<>();
 
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
@@ -22,4 +24,10 @@ public class FileReader {
         }
         return fileContent;
     }
+
+    public static LocalDate setLocalDate(String date, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return LocalDate.parse(date, formatter);
+    }
+
 }
